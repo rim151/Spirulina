97c268f578aaf9003fb783cf79398a1bec0aec7d
Nykka clone <rimjhimverma179@gmail.com>
2026-01-10 13:24:47 +0530

    push

--- spirulina-dashboard/src/main.jsx
+++ spirulina-dashboard/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
